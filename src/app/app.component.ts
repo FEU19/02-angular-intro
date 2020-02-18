6d25915a8028051bef5538a9055ca30d0aecc862
David Andersson <david.andersson@zocom.se>
2020-02-18 10:07:38 +0100

    Class properties and click events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'heffaklump',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'heffaklump',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-intro';
+export class AppComponent implements OnInit {
+	title = 'Angular demo';
+	flavor: string = 'chocolate';
+	optimumAmount: number = 3;
+
+	makeItStrawberry(): void {
+		console.log('makeItStrawberry');
+		this.flavor = 'strawberry';
+	}
+	restoreChocolate(): void {
+		this.flavor = 'chocolate';
+	}
+
+	// use OnInit to do things when the component is first created
+	ngOnInit(): void {
+		console.log('ngOnInit: AppComponent was created');
+		// this.flavor = 'strawberry';
+	}
 }
+
+
+
+
+
+
+//
